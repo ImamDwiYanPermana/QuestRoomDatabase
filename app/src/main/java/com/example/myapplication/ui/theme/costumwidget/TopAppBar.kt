c7a6aa6488c9e3f6c4a1b6270b04f4933b3bbe8e
ImamDwiYanPermana <imampermana244@gmail.com>
2024-12-16 19:21:24 +0700

    Menghapus import

--- app/src/main/java/com/example/myapplication/ui/theme/costumwidget/TopAppBar.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/costumwidget/TopAppBar.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication.ui.theme.costumwidget
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.FlowRowScopeInstance.align
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
