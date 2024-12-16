cd9c37c74f43180f40110eff3439d7feeb756aec
ImamDwiYanPermana <imampermana244@gmail.com>
2024-12-16 19:20:39 +0700

    Menambahkan Text Untuk Judul

--- app/src/main/java/com/example/myapplication/ui/theme/costumwidget/TopAppBar.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/costumwidget/TopAppBar.kt
@@ -1,12 +1,19 @@
 package com.example.myapplication.ui.theme.costumwidget
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.FlowRowScopeInstance.align
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TopAppbar(
@@ -18,8 +25,28 @@ fun TopAppbar(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp),
-        contentAlignment = Alignment.Center
-    ){
+        contentAlignment = Alignment.Center // Pastikan konten di tengah
+    ) {
+        if (showBackButton) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextButton(
+                    onClick = onBack,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                ) {
+                    Text("Kembali")
+                }
+                Spacer(modifier = Modifier.weight(2f))
+            }
+        }
 
+        Text(
+            text = judul,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.Center)
+        )
     }
 }
