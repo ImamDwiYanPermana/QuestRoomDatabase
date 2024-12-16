838409eeedc6c44e212667eaee3f4a058054340b
ImamDwiYanPermana <imampermana244@gmail.com>
2024-12-16 18:59:31 +0700

    Menambahkan fungsi absatrak yang itu override dan di tunggu oleh susspend karena melibatkan database dan membuat override fun untuk mengabil data yang di butuhkan

--- app/src/main/java/com/example/myapplication/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/myapplication/repository/LocalRepositoryMhs.kt
@@ -2,9 +2,26 @@ package com.example.myapplication.repository
 
 import com.example.myapplication.data.dao.MahasiswaDao
 import com.example.myapplication.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
-class LocalRepositoryMhs (private val mahasiswaDao: MahasiswaDao) :RepositoryMhs {
-    override suspend fun insertMhs(mahasiswa: Mahasiswa){
+class LocalRepositoryMhs (
+    private val mahasiswaDao: MahasiswaDao
+) : RepositoryMhs {
+
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> =
+        mahasiswaDao.getAllMahasiswa()
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> =
+        mahasiswaDao.getMahasiswa(nim)
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
