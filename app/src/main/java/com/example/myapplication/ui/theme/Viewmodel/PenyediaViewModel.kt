021db0dd218f40d8c42f979ecec28a63ede647f2
ImamDwiYanPermana <imampermana244@gmail.com>
2024-12-17 12:12:39 +0700

    Membuat initializer untuk menganalisi setiap MahasiswaHomeView Model dan Detail beserta Updare

--- app/src/main/java/com/example/myapplication/ui/theme/Viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Viewmodel/PenyediaViewModel.kt
@@ -2,16 +2,34 @@ package com.example.myapplication.ui.theme.Viewmodel
 
 import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.sqlite.db.SupportSQLiteOpenHelper
 import com.example.myapplication.KrsApp
 
-object PenyediaViewModel{
+object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            MahasiswaHomeViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            updateMhsViewModel(
+                createSavedStateHandle(),
                 krsApp().containerApp.repositoryMhs
             )
         }
@@ -19,4 +37,4 @@ object PenyediaViewModel{
 }
 
 fun CreationExtras.krsApp(): KrsApp =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as KrsApp)
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
